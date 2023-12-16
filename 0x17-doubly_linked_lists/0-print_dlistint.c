f60861d86faa2252acc193e4c7c00ad9d29a58f0
pitsomnk <pitsomnk@gmail.com>
2023-12-16 11:35:06 -0800

    another commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,22 @@
 #include "lists.h"
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
+	int count;
 
-	/* while we have a pointer to the list */
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
+
 	return (count);
 }
