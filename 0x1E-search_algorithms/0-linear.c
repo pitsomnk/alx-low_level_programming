02d73c54c7d93dc1911e347fe3f0027501115a82
Pitso Manyike <pitsomnk@gmail.com>
2024-07-18 11:19:19 -0700

    another commit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,21 +1,19 @@
-#include "search_algos.h"
+#include <stddef.h>
 
 /**
  * linear_search - searches for a value in an array of
- * integers using the Linear search algorithm
+ * integers using the Linear search algoritm
  */
-int linear_search(int *array, size_t size, int value)
-{
-	int i;
-
-	if (array == NULL)
-		return (-1);
-
-	for (i = 0; i < (int)size; i++)
-	{
-		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (value == array[i])
-			return (i);
-	}
-	return (-1);
+int linear_search(int *array, size_t size, int value) {
+    if (array == NULL) {
+        return -1;
+    }
+    
+    for (size_t i = 0; i < size; i++) {
+        if (array[i] == value) {
+            return i;
+        }
+    }
+    
+    return -1;
 }
