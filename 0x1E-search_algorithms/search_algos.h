e8185e2d8231e15575ed5e4a77f4812e7786836d
Pitso Manyike <pitsomnk@gmail.com>
2024-07-18 14:52:13 -0700

    another

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,4 +10,12 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 
+
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+    listint_t *jump_list(listint_t *list, size_t size, int value);
+} listint_t;
 #endif
