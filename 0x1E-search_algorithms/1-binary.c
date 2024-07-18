34cbebd38098dd4f527509bca5807b3426096dca
Pitso Manyike <pitsomnk@gmail.com>
2024-07-18 13:14:12 -0700

    another one

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,7 @@
 #include "search_algos.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 /**
   * binary_search - Searches for a value in a sorted array
   *                 of integers using binary search.
